3c2ad1561c57c4f147a36e5cfa73062c19a817a0
lakhichand <lakhichand@gmail.com>
2024-08-16 14:22:30 +0530

    Basic_proj

---
+++ Basic-proj/src/org/jspiders/basicApp/Test.java
@@ -0,0 +1,10 @@
+package org.jspiders.basicApp;
+
+public class Test {
+
+public static void main(String[] args) {
+		
+		Trainer t = new Trainer();
+		t.shout();
+	}
+}
---
+++ Basic-proj/src/org/jspiders/basicApp/Trainer.java
@@ -0,0 +1,8 @@
+package org.jspiders.basicApp;
+
+public class Trainer {
+
+	void shout() {
+		System.out.println("Silence...!! silence...!!");
+	}
+}
