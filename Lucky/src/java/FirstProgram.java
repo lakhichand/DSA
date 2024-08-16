9f702f57dc677663fae10e3ed250ab3c78edd538
lakhichand <lakhichand@gmail.com>
2024-08-16 15:02:13 +0530

    Lucky

---
+++ Lucky/src/java/FirstProgram.java
@@ -0,0 +1,11 @@
+package java;
+
+public class FirstProgram {
+   
+	public static void main(String[] args) {
+		
+		String s = ("hello world");
+		System.out.println(s);
+		System.out.println("hello world !");
+	}
+}
---
+++ Lucky/src/java/var1.java
@@ -0,0 +1,13 @@
+package java;
+
+public class var1 {
+
+	public static void main(String[] args) {
+		
+		float f;
+		
+		f = 45678.12f;
+		
+		System.out.println(f);
+	}
+}
